760fce0d64158a11970dcfc18e5904f1d2665ab4
Peter <peter.nels.hall@gmail.com>
2023-12-08 19:40:38 -0700

    changed circle

--- gap_follow/scripts/reactive_node.py
+++ gap_follow/scripts/reactive_node.py
@@ -28,7 +28,7 @@ class ReactiveFollowGap(Node):
 
         self.max_distance = 5.0
         self.average_window = 5
-        self.obs_rad = 0.5
+        self.obs_rad = 3.0
         self.car_rad = 50
         self.target_distance = 1.5
         self.speed = 0.5
@@ -93,6 +93,9 @@ class ReactiveFollowGap(Node):
             if i - tmp_start > max_width:
                     start = tmp_start
                     end = i
+
+        if count == 0:
+            print("NO POSITIVES")
 
         return start, end
             
